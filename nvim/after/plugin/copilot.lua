b7535a0270df81098047e7e54094ffe70f226cf9
Jordan <jlaprise@blakwurm.com>
2023-10-09 09:26:14 -0800

    General updates

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,3 +1,3 @@
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true }) 
+-- vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<C-j>")', { silent = true, expr = true }) 
 vim.g.copilot_enabled = 1
